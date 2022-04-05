5fb823ef9fbf03bad830e4685d22727b8774d1d1
MatheusBeltrao <57968421+MatheusBeltrao@users.noreply.github.com>
2022-04-04 23:14:09 -0300

    Add files via upload

---
+++ tarefa.java
@@ -0,0 +1,41 @@
+
+public class tarefa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		    System.out.println("raiz: "+Math.pow(64, 0.5));
+			
+			System.out.println("raiz com exp e log e realint: "+(int)Math.sin(64*0.5));
+			
+			System.out.println("raiz com exp e log sem realint: "+Math.sin(64*0.5));
+			
+			System.out.println("formatar "+Math.sin(45*3.14/180));
+			
+			System.out.println("potencia com operador ** e formatar: "+Math.pow(8,3.3));
+			
+			System.out.println("raiz cubica: "+Math.pow(8,0.333));
+			
+			System.out.println("absoluto "+Math.abs(-8));
+			
+			System.out.println("absoluto "+Math.abs(8));
+			
+			System.out.println("convertendo para real: "+(double)87);
+			
+			System.out.println("convertendo para inteiro 5.5: "+(int)5.5);
+			
+			System.out.println("convertendo para inteiro 6.5: "+(int)6.5);
+			
+			System.out.println("convertendo para inteiro 6.5+0.0001: "+(int)6.5+0.0001);
+			
+			System.out.println("convertendo para inteiro 5.45: "+(int)5.45);
+			
+			System.out.println("convertendo para inteiro 5.51: "+(int)5.51);
+				
+			System.out.println("convertendo para real 87: "+(double)87);
+			
+			System.out.println("convertendo para inteiro:"+(int)3/4);
+	        
+	}
+
+}
